8c0ce18fa960d4139c5132fb593cb2b35b013e81
shangyulin <shangyulin@ztgame.com>
2018-11-30 15:18:18 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+// StatelessWidget是不可变的，属性固定
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        // app的主题
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: new RandomWord());
+  }
+}
+
+class RandomWord extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return new RandomWordState();
+  }
+}
+
+class RandomWordState extends State<RandomWord> {
+  final _suggests = <WordPair>[];
+  final _saved = Set<WordPair>();
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Welcome to flutter"),
+        centerTitle: true,
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        // itemBuilder没调用一次自增1
+        itemBuilder: (context, i) {
+          if (i.isOdd) return new Divider();
+          final index = i ~/ 2;
+          if (index >= _suggests.length) {
+            _suggests.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggests[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: (){
+        // setState()会触发State对象的build方法，以此来刷新UI
+        setState(() {
+          if(alreadySaved){
+            _saved.remove(pair);
+          }else{
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+}
+
+//class MyHomePage extends StatefulWidget {
+//  MyHomePage({Key key, this.title}) : super(key: key);
+//  final String title;
+//  @override
+//  _MyHomePageState createState() => _MyHomePageState();
+//}
+//
+//class _MyHomePageState extends State<MyHomePage> {
+//  int _counter = 0;
+//
+//  void _incrementCounter() {
+//    setState(() {
+//      _counter++;
+//    });
+//  }
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      appBar: AppBar(
+//        title: Text(widget.title),
+//      ),
+//      body: Center(
+//        child: Column(
+//          mainAxisAlignment: MainAxisAlignment.center,
+//          children: <Widget>[
+//            Text(
+//              'hahaha You have click the button this many times:',
+//            ),
+//            Text(
+//              '$_counter',
+//              style: Theme.of(context).textTheme.display1,
+//            ),
+//          ],
+//        ),
+//      ),
+//      floatingActionButton: FloatingActionButton(
+//        onPressed: _incrementCounter,
+//        tooltip: 'Increment',
+//        child: Icon(Icons.add),
+//      ),
+//    );
+//  }
+//}
